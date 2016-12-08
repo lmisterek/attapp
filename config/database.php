19607f656de7c944b736d41c14e8b52c3a705335
LeeAnna Misterek <leeanna.misterek@gmail.com>
2016-12-08 00:22:10 -0700

    changed pathway for unix_socket

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'unix_socket'   => '/tmp/mysql.sock',
+            'unix_socket'   => '/var/run/mysqld/mysqld.sock',
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'attapp'),
             'username' => env('DB_USERNAME', ''),
